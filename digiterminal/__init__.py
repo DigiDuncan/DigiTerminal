1bd27b92b8288785cd7dce8df0bdd60cced34c10
DigiDuncan <digiduncan@gmail.com>
2020-05-11 13:40:53 -0400

    Move utils to utils.

--- digiterminal/__init__.py
+++ digiterminal/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.1.0"
 
-__all__ = ["cursorUp", "cursorDown", "cursorRight", "cursorLeft", "scrollUp", "scrollDown", "setWindowTitle", "overwriteLines", "createLoadBar", "truncate", "styles"]
+__all__ = ["cursorUp", "cursorDown", "cursorRight", "cursorLeft", "scrollUp", "scrollDown", "setWindowTitle", "overwriteLines", "createLoadBar", "truncate", "styles", "goto"]
 
-from digiterminal.formatter import cursorUp, cursorDown, cursorRight, cursorLeft, scrollUp, scrollDown, setWindowTitle, overwriteLines, createLoadBar, truncate, goto
+from digiterminal.formatter import cursorUp, cursorDown, cursorRight, cursorLeft, scrollUp, scrollDown, setWindowTitle, overwriteLines, goto
 from digiterminal.styles import styles
+from digiterminal.utils import createLoadBar, truncate
--- digiterminal/formatter.py
+++ digiterminal/formatter.py
@@ -1,7 +1,6 @@
 import os
-import math
 
-__all__ = ["cursorUp", "cursorDown", "cursorRight", "cursorLeft", "scrollUp", "scrollDown", "setWindowTitle", "overwriteLines", "createLoadBar", "truncate"]
+__all__ = ["cursorUp", "cursorDown", "cursorRight", "cursorLeft", "scrollUp", "scrollDown", "setWindowTitle", "overwriteLines"]
 
 # Activate VT-100 terminal formatting
 os.system("")
@@ -105,44 +104,3 @@ def overwriteLines(lines, hack = False):
         if li != lines - 1:
             print(CURSOR_UP)
     print(BEGIN_OF_LINE)
-
-
-def createLoadBar(current, total, barlength = 50, showpercent = False):
-    """Create a progress bar"""
-    TWENTYFIVE = "\u2591"
-    FIFTY = "\u2592"
-    SEVENTYFIVE = "\u2593"
-    FULL = "\u2588"
-    shades = {
-        0: " ",
-        1: TWENTYFIVE,
-        2: FIFTY,
-        3: SEVENTYFIVE,
-        4: FULL
-    }
-
-    complete = current / total          # fraction complete
-    bartodraw = complete * barlength    # how long the bar should be
-
-    fullbar = math.floor(bartodraw)     # how many full segments the bar should have
-    fullbarstring = FULL * fullbar
-
-    remainder = bartodraw - fullbar     # fraction remaining to draw
-    shade = math.floor(remainder * 4)   # what shade the last segment should be
-    remainderstring = shades[shade]
-
-    barstring = fullbarstring + remainderstring
-    if showpercent:
-        percentage = round(complete * 100, 2)
-        barstring = f"{barstring} {percentage}%"
-    return barstring
-
-
-def truncate(s, trunclen = 80, trailing = False):
-    """Truncate a long string for terminal printing"""
-    if len(s) > trunclen:
-        if trailing:
-            s = "…" + s[-(trunclen - 1):]
-        else:
-            s = s[(trunclen - 1):] + "…"
-    return s
